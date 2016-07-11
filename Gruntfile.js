103c9f3816329546f0160edac25492d8b5e62818
ChrisHeo <christopher.s.heo@gmail.com>
2016-07-11 18:43:53 -0400

    final version

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,62 @@
+
 module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-nodemon');
+  ////////////////////////////////////////////////////
+  // Main grunt tasks
+  ////////////////////////////////////////////////////
+
+  grunt.registerTask('test', [
+    'mochaTest', 'jshint'
+  ]);
+
+  grunt.registerTask('build', [
+    'concat', 'uglify', 'cssmin'
+  ]);
+
+  grunt.registerTask('upload', function(n) {
+    if(grunt.option('prod')) {
+      grunt.task.run(['shell:prodServer'])
+    } else {
+      grunt.task.run([ 'server-dev' ]);
+    }
+  });
+
+  grunt.registerTask('deploy', [
+    'jshint', 'mochaTest', 'concat', 'uglify', 'cssmin', 'shell'
+  ]);
+
+  grunt.registerTask('heroku:production', [
+      'build'
+  ]);
+
+  grunt.registerTask('server-dev', function (target) {
+    // Running nodejs in a different process and displaying output on the main console
+    var nodemon = grunt.util.spawn({
+         cmd: 'grunt',
+         grunt: true,
+         args: 'nodemon'
+    });
+    nodemon.stdout.pipe(process.stdout);
+    nodemon.stderr.pipe(process.stderr);
+    grunt.task.run([ 'watch' ]);
+  });
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     concat: {
+       dist: {
+        src: [
+            'public/client/*.js'  //all JS files in public/client
+        ],
+        dest: 'public/temp/production.js'
+      }
     },
 
     mochaTest: {
@@ -21,14 +75,22 @@ module.exports = function(grunt) {
     },
 
     uglify: {
+      build: {
+        src: 'public/temp/production.js',
+        dest: 'public/dist/client/production.min.js'
+      }
     },
 
     jshint: {
       files: [
-        // Add filespec list here
+        'public/**/*.js',
+        'app/**/*.js',
+        'lib/**/*.js',
+        './*.js'
       ],
       options: {
         force: 'true',
+        reporterOutput: '',
         jshintrc: '.jshintrc',
         ignores: [
           'public/lib/**/*.js',
@@ -38,6 +100,15 @@ module.exports = function(grunt) {
     },
 
     cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'public',
+          src: ['*.css', '!*.min.css'],
+          dest: 'public/dist',
+          ext: '.min.css'
+        }]
+      }
     },
 
     watch: {
@@ -59,55 +130,8 @@ module.exports = function(grunt) {
 
     shell: {
       prodServer: {
+        command: 'git push heroku master'
       }
     },
   });
-
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-mocha-test');
-  grunt.loadNpmTasks('grunt-shell');
-  grunt.loadNpmTasks('grunt-nodemon');
-
-  grunt.registerTask('server-dev', function (target) {
-    // Running nodejs in a different process and displaying output on the main console
-    var nodemon = grunt.util.spawn({
-         cmd: 'grunt',
-         grunt: true,
-         args: 'nodemon'
-    });
-    nodemon.stdout.pipe(process.stdout);
-    nodemon.stderr.pipe(process.stderr);
-
-    grunt.task.run([ 'watch' ]);
-  });
-
-  ////////////////////////////////////////////////////
-  // Main grunt tasks
-  ////////////////////////////////////////////////////
-
-  grunt.registerTask('test', [
-    'mochaTest'
-  ]);
-
-  grunt.registerTask('build', [
-
-  ]);
-
-  grunt.registerTask('upload', function(n) {
-    if(grunt.option('prod')) {
-      // add your production server task here
-    } else {
-      grunt.task.run([ 'server-dev' ]);
-    }
-  });
-
-  grunt.registerTask('deploy', [
-    // add your deploy tasks here
-  ]);
-
-
 };
--- app/models/link.js
+++ app/models/link.js
@@ -1,19 +1,45 @@
-var db = require('../config');
 var crypto = require('crypto');
+var mongo = require('mongoose');
 
-var Link = db.Model.extend({
-  tableName: 'urls',
-  hasTimestamps: true,
-  defaults: {
-    visits: 0
-  },
-  initialize: function(){
-    this.on('creating', function(model, attrs, options){
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-  }
+var linkSchema = mongo.Schema({
+  visits: Number,
+  link: String,
+  title: String,
+  code: String,
+  base_url: String,
+  url: String
 });
 
+var Link = mongo.model('Link', linkSchema);
+
+var encrypt = function(url){
+  var shasum = crypto.createHash('sha1');
+  shasum.update('url');
+  return shasum.digest('hex').slice(0, 5);
+};
+
+linkSchema.pre('save', function(next){
+  var code = encrypt(this.url);
+  this.code = code;
+  next();
+})
+
 module.exports = Link;
+
+
+// var Link = db.Model.extend({
+//   tableName: 'urls',
+//   hasTimestamps: true,
+//   defaults: {
+//     visits: 0
+//   },
+//   initialize: function(){
+//     this.on('creating', function(model, attrs, options){
+//       var shasum = crypto.createHash('sha1');
+//       shasum.update(model.get('url'));
+//       model.set('code', shasum.digest('hex').slice(0, 5));
+//     });
+//   }
+// });
+
+// module.exports = Link;
